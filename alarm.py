3df24c65f54b9d7876416f863ea919d788095549
alexandre <alexandre.chevalier@laplateforme.io>
2025-01-07 10:44:22 +0100

    code pour set up l'alarme et affichage d'un message modification a venir

---
+++ alarm.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+import time
+import random
+
+hours = [random.randint(0,23),random.randint(0,59),random.randint(0,59)]
+
+def setAlarm():
+    
+    hour = int(input("enter the hour you want for the alarm "))
+    if hour > 23:
+        setAlarm()
+    else:
+        minutes = int(input("enter the minutes you want for the alarm "))
+        seconds = int(input("enter the seconds you want for the alarm "))
+        if minutes >59 and minutes>59:
+            setAlarm()
+        else:
+            link_alarm=[hour,minutes, seconds]
+            print(link_alarm)
+            return link_alarm
+
+        
+        
+    
+
+def alarm(time, time_alarm):
+    print(time)
+    print(time_alarm)
+    
+    times   = f"{time[0]}:{time[1]}:{time[2]}"
+    alarms  = f"{time_alarm[0]}:{time_alarm[1]}:{time_alarm[2]}"
+    format_alarms = "%H:%M:%S"
+    
+    current_times   = datetime.strptime(times,format_alarms) 
+    alarm_target    = datetime.strptime(alarms, format_alarms)
+    print(alarm_target, current_times)
+
+    
+    if current_times != alarm_target:
+
+    else:
+        print(f"il est {time_alarm[0]}:{time_alarm[1]}:{time_alarm[2]}  bip biiip biiiiip BIIIIIIIIIIIP votre alarme sonne")
+        
+            
+
+alarm(hours, setAlarm())
